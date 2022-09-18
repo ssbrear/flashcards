1dfaf52e0db615163934dfaeb2ca328fcec2f72f
Sebastian Brear <ssbrear@ucdavis.edu>
2022-09-17 17:23:36 -0700

    Included an additional arrow that appears above a word once a category has been selected to 'unselect' that category and return to category selection

--- public/scripts/modules/addArrows.js
+++ public/scripts/modules/addArrows.js
@@ -22,6 +22,18 @@ function addArrows() {
       if (currentWord.previousSibling != null) currentWord.previousSibling.classList.add("active");
     });
     wordContainers[i].appendChild(prevArrow);
+
+    const upArrow = document.createElement("span");
+    upArrow.setAttribute("class", "upArrow");
+    upArrow.textContent = "↑";
+    upArrow.addEventListener("click", function (e) {
+      e.stopPropagation();
+      let currentWord = document.querySelectorAll(".word.active")[0];
+      currentWord.classList.remove("active");
+      document.getElementById("categories").classList.remove("active");
+      document.querySelectorAll(".category.active")[0].classList.remove("active");
+    });
+    wordContainers[i].appendChild(upArrow);
   }
 }
 
